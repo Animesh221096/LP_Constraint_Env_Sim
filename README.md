8a99ae639c024aeccf63dce2a651dcf62ccfbebf
Partha S. Dutta <thetechguy1092@gmail.com>
2025-11-27 15:34:19 +0530

    Created Environment

---
+++ README.md
@@ -0,0 +1,35 @@
+```
+git clone https://github.com/QTrino-Labs-Pvt-Ltd/C_Env_Sim.git
+cd HSM_BETA/
+```
+
+```
+python3 -m venv litex-env
+source litex-env/bin/activate
+```
+
+```
+chmod +x litex_setup.py
+```
+
+```
+./litex_setup.py --init --install
+```
+
+```
+pip3 install meson ninja
+```
+
+```
+sudo ./litex_setup.py --gcc=riscv
+```
+
+```
+sudo apt install libevent-dev libjson-c-dev verilator
+```
+
+```
+litex_sim --csr-json csr.json --cpu-type=vexriscv --cpu-variant=full --uart-baudrate=1000000 --integrated-main-ram-size=0x06400000 --no-start
+litex_bare_metal_demo --build-path=build/sim
+litex_sim --csr-json csr.json --cpu-type=vexriscv --cpu-variant=full --uart-baudrate=1000000 --integrated-main-ram-size=0x06400000 --ram-init=boot.bin
+```
---
+++ boot/main.c
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>  
+
+#include <irq.h>
+#include <libbase/uart.h>
+#include <libbase/console.h>
+#include <generated/csr.h>
+#include <wolfssl/wolfcrypt/user_settings.h>
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/wc_mlkem.h>
+#include <wolfssl/wolfcrypt/random.h>
+
+// static FATFS fs;//File system object to use Fatfs
+
+#ifdef min
+#undef min
+#endif
+
+#ifdef max
+#undef max
+#endif
+
+
+
+//-----------------------------Wolf required stubs--------------------------------------
+#include <wolfssl/wolfcrypt/types.h>
+
+/* Replace this with a real HW entropy source (TRNG on FPGA if available) */
+int CustomRngGenerateBlock(byte *output, word32 sz) {
+    for (word32 i = 0; i < sz; i++) {
+        output[i] = (byte)(i * 37 + 123); // placeholder (NOT SECURE!)
+    }
+    return 0;
+}
+
+#include <sys/time.h>
+#include <time.h>
+
+/* return 0 time, the time of UNIX epoch */
+int gettimeofday(struct timeval* tv, void* tz) {
+    if (tv) {
+        tv->tv_sec = 0;
+        tv->tv_usec = 0;
+    }
+    return 0;
+}
+//-----------------------------Wolf required stubs--------------------------------------//
+
+WC_RNG global_rng;
+int main(void)
+{
+#ifdef CONFIG_CPU_HAS_INTERRUPT
+	irq_setmask(0);
+	irq_setie(1);
+#endif
+	uart_init();
+    printf("\nTEST Start\n");
+    
+    int ret;
+    wc_InitRng(&global_rng); 
+
+    uint8_t buffer[32];
+    // Generate random bytes
+    ret = wc_RNG_GenerateBlock(&global_rng, buffer, 32);
+    if (ret != 0) { 
+        printf("Random generation failed, ret = %d\n", ret);
+        wc_FreeRng(&global_rng);
+        return 1;
+    }
+
+    // Print random bytes
+    printf("Generated %d random bytes:\n", 32);
+    for (int i = 0; i < 32; i++) {
+        printf("%02X", buffer[i]);
+        if ((i + 1) % 16 == 0)
+            printf("\n");
+        else
+            printf(" ");
+    }
+
+    printf("--------------------------ML_KEM_512-------------------------\n");
+	MlKemKey* Bench_key = wc_MlKemKey_New(WC_ML_KEM_512, NULL, INVALID_DEVID);
+    if (wc_MlKemKey_MakeKey(Bench_key, &global_rng) != 0) printf("Error: MlKem key creation failed.\n");
+
+    #define PUB_SIZE  WC_ML_KEM_512_PUBLIC_KEY_SIZE
+    #define PRIV_SIZE WC_ML_KEM_512_PRIVATE_KEY_SIZE
+ 
+    unsigned char pubK[PUB_SIZE];
+    unsigned char privK[PRIV_SIZE];
+
+    // Encode public key 
+    ret = wc_MlKemKey_EncodePublicKey(Bench_key, pubK, PUB_SIZE);
+    if (ret != 0) {
+        printf("Public key encode failed: %d\n", ret);
+    }
+
+    // Encode private key
+    ret = wc_MlKemKey_EncodePrivateKey(Bench_key, privK, PRIV_SIZE);
+    if (ret != 0) {
+        printf("Private key encode failed: %d\n", ret);
+    }
+
+    printf("\nPrivate key\n");
+    for (int i = 0; i < PRIV_SIZE; i++) {
+        printf("%02x", privK[i]);
+    }
+    printf("\nPublic key\n");
+    for (int i = 0; i < PUB_SIZE; i++) {
+        printf("%02x", pubK[i]);
+    }
+    printf("\n\n");
+
+    wc_MlKemKey_Free(Bench_key);
+
+    return 0;
+}
+
+
+
+
+
+
+//     /* Initialize SDCard */
+//     // sdcard_init();
+    // spisdcard_init();
+//     printf("sdcard_init() done\n");
+
+//     /* Bind FatFS to SPI backend */
+    // fatfs_set_ops_spisdcard();
+//     printf("fatfs_set_ops_sdcard() done\n");
+    
+//     /* Mount filesystem */
+//     FRESULT fr = f_mount(&fs, "", 1);  //MS-DOS partition table + FAT32 Filesystem
+//     printf("f_mount -> %d\n", fr);
+//     if (fr != FR_OK) {
+//         printf("mount failed\n");
+//         return 1;
+//     }
+
+//     FIL file;
+//     UINT bytes=0;
+
+//     /* PHASE 1 — READ existing file */
+//     printf("\n[PHASE 1] Opening test.txt for READ...\n");
+//     fr = f_open(&file, "Work.txt", FA_READ | FA_OPEN_EXISTING);
+//     printf("[PHASE 1] f_open -> %d\n", fr);
+//     if (fr != FR_OK) return 1;
+
+//     static char buf[4096];
+//     memset(buf, 0, sizeof(buf));
+
+//     fr = f_read(&file, buf, sizeof(buf)-1, &bytes);
+//     printf("[PHASE 1] f_read -> %d, bytes=%u\n", fr, bytes);
+//     printf("[PHASE 1] Contents BEFORE write:\n%s\n", buf);
+
+//     f_close(&file);
+//     bytes=0;
+
+//     /* PHASE 2 — file write */
+//     printf("\n[PHASE 2] Opening test.txt for WRITE + TRUNCATE...\n");
+//     fr = f_open(&file, "Work.txt", FA_WRITE | FA_OPEN_EXISTING);
+//     printf("[PHASE 2] f_open -> %d\n", fr);
+//     if (fr != FR_OK) return 1;
+
+//     f_lseek(&file, f_size(&file));
+//     // f_truncate(&file); 
+
+//     const char *msg = "\nHELLO TO THE WORLD\n";
+//     fr = f_write(&file, msg, strlen(msg), &bytes); 
+//     printf("[PHASE 2] f_write -> %d, bytes=%u\n", fr, bytes);
+ 
+//     f_close(&file);// IT FAILS HERE
+//     bytes=0;
+
+//     /* PHASE 3 — Read again */
+//     printf("\n[PHASE 3] Opening test.txt for READ again...\n");
+//     fr = f_open(&file, "Work.txt", FA_READ | FA_OPEN_EXISTING);
+//     printf("[PHASE 3] f_open -> %d\n", fr);
+//     if (fr != FR_OK) return 1;
+
+//     memset(buf, 0, sizeof(buf));
+
+//     fr = f_read(&file, buf, sizeof(buf)-1, &bytes);
+//     printf("[PHASE 3] f_read -> %d, bytes=%u\n", fr, bytes);
+//     printf("[PHASE 3] Contents AFTER write:\n%s\n", buf);
+
+//     f_close(&file);
+
+//     printf("\n=== SD TEST COMPLETE ===\n");
